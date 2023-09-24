1e216cce64f9a02cf702212478ae6c5f4fee7127
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-09-25 00:34:19 +0200

    Stuff

---
+++ Kthura_NewProjectWizard/Stuff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrickyUnits;
+
+namespace Kthura_NewProjectWizard {
+	static internal class Stuff {
+		internal const string upGlobalConfigFile = "$AppSupport$/KthuraMapEditor.Config.INI";
+		static internal string GlobalConfigFile => Dirry.C(upGlobalConfigFile).Replace('\\','/');
+
+		static internal GINIE GlobalConfig = null;
+
+		static internal GINIE Project = GINIE.FromSource("# Nothing to see here");
+
+		static Stuff() {
+			Debug.WriteLine($"Loading: {GlobalConfigFile}");
+			GlobalConfig = GINIE.FromFile(GlobalConfigFile);
+		}
+	}
+}
