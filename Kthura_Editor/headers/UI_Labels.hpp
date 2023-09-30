fd0545a9e5c3c412ecce3beffd23b2ea8e3352f9
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2023-09-30 16:43:54 +0200

    Template for labels

---
+++ Kthura_Editor/headers/UI_Labels.hpp
@@ -0,0 +1,34 @@
+// Lic:
+// Kthura
+// Labels (header)
+// 
+// 
+// 
+// (c) Jeroen P. Broks, 2015, 2016, 2017, 2019, 2021, 2023
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// Please note that some references to data like pictures or audio, do not automatically
+// fall under this licenses. Mostly this is noted in the respective files.
+// 
+// Version: 23.09.30
+// EndLic
+
+#pragma once
+namespace Slyvina {
+	namespace Kthura {
+		namespace Editor {
+
+		}
+	}
+}
