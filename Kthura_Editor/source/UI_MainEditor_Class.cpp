e2ffe2b91e2e7395e036e5497c5fd9a9b7d8408d
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-02-18 17:09:43 +0100

    vUpdate

--- Kthura_Editor/source/UI_MainEditor_Class.cpp
+++ Kthura_Editor/source/UI_MainEditor_Class.cpp
@@ -4,7 +4,7 @@
 // 
 // 
 // 
-// (c) Jeroen P. Broks, 2015, 2016, 2017, 2019, 2021, 2023
+// (c) Jeroen P. Broks, 2015, 2016, 2017, 2019, 2021, 2023, 2024
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 23.10.01
+// Version: 24.02.18
 // EndLic
 
 #include <TQSE.hpp>
@@ -390,6 +390,7 @@ namespace Slyvina {
 				Amber(Tag);
 				Tag->Enabled = false;
 				Tag->CBAction = TagButton;
+
 
 			}
 #pragma endregion
