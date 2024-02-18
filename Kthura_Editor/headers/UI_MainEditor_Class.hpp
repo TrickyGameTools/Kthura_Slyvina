0091f990c907ae9bcdae877adc9d7ea93b74fb9e
Jeroen Broks <11202073+Tricky1975@users.noreply.github.com>
2024-02-18 17:05:55 +0100

    Not important

--- Kthura_Editor/headers/UI_MainEditor_Class.hpp
+++ Kthura_Editor/headers/UI_MainEditor_Class.hpp
@@ -4,7 +4,7 @@
 // 
 // 
 // 
-// (c) Jeroen P. Broks, 2015, 2016, 2017, 2019, 2021, 2023
+// (c) Jeroen P. Broks, 2015, 2016, 2017, 2019, 2021, 2023, 2024
 // 
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -21,7 +21,7 @@
 // Please note that some references to data like pictures or audio, do not automatically
 // fall under this licenses. Mostly this is noted in the respective files.
 // 
-// Version: 23.09.29
+// Version: 24.02.18
 // EndLic
 
 #pragma once
@@ -92,6 +92,8 @@ namespace Slyvina {
 					* AutoTexIns{ nullptr },
 					* Visible{ nullptr },
 					* Labels{ nullptr }, * Tag{ nullptr };
+					//* AutoInsert{ nullptr };
+			
 			};
 
 			typedef void(*UIEMouseCB)(int X, int Y);
